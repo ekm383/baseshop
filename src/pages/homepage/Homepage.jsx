11d40472556ec1b15b156338643286555ca7a7e7
Eric Manongdo <superscript@Erics-MBP.home>
2020-12-16 16:36:40 -1000

    Base Shop no state management

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Directory from "../../components/directory/directory";
+
+import "./homepage.scss";
+
+const Homepage = () => {
+  return <Directory />;
+};
+
+export default Homepage;
